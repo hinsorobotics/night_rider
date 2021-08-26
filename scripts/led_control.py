660bf013d447c8e4f360b30ed2b849aff6c31764
Arron-Griffiths <81839171+Arron-Griffiths@users.noreply.github.com>
2021-08-26 10:16:40 -0400

    add the basic ROS driver - In development

---
+++ scripts/led_control.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#-------------- led_control.py --------------------------------------------------
+# Arthor: Arron Griffiths | Company: Indro Robotics | Location: Ottawa, ON, Canada
+# Contact: agriffiths@indrorobotics.com  
+# Info: This is a simple python ros node that connects to a serial port and pushes 
+#       out a string of information define by the numato Lab 8 channel relay 
+#       controller.
+#       This ros node has been configured for the indro robotics "night rider" 
+#       platform, to allow control of the leds via ros service calls
+#------------------------------------------------------------------------------
+
+import rospy
+import serial
+from std_srvs.srv import SetBool, SetBoolResponse
+from std_msgs.msg import ByteMultiArray
+
+
+class LedControl:
+    def __init__(self):
+#-------------- Serial setup --------------------------------------------------
+        self.port = rospy.get_param('~port', '/dev/ttyACM3')
+        self.baud = rospy.get_param('~baud', 19200)
+        self.serial_port = serial.Serial(self.port, self.baud, timeout=1)
+#-------------- function deleceration -----------------------------------------
+        rospy.Service('set_rear_red_led', SetBool, self.control_led_0)
+        rospy.Service('set_rear_spot_leds', SetBool, self.control_led_1)
+        rospy.Service('set_rear_left_yellow_led', SetBool, self.control_led_2)
+        rospy.Service('set_rear_right_yellow_led', SetBool, self.control_led_3)       
+        rospy.Service('set_front_left_yellow_led', SetBool, self.control_led_4)
+        rospy.Service('set_front_right_yellow_led', SetBool, self.control_led_5)
+        rospy.Service('set_front_spot_leds', SetBool, self.control_led_6)
+        rospy.Service('set_spare_led', SetBool, self.control_led_7)
+        rospy.Service('set_all_leds', SetBool, self.control_all_leds)
+        rospy.Service('set_right_yellow_leds', SetBool, self.control_right_yellow_leds)
+        rospy.Service('set_left_yellow_leds', SetBool, self.control_left_yellow_leds)
+        
+        rospy.Subscriber("/led_light_control", ByteMultiArray, self.callbackSetLEDs)
+        
+#-------------- ROS Spin loop -------------------------------------------------
+        while not rospy.is_shutdown():
+            rospy.spin()
+
+        self.serial_port.close()
+
+
+#-------------- function deleceration -----------------------------------------
+    def control_relay_write_read(self, req, relay):
+        if(req.data):
+            cmd = str("on")
+        else:
+            cmd = str("off")
+        self.serial_port.write("relay " + cmd + " " + str(relay) + "\n\r")
+        self.serial_port.flush()
+        self.serial_port.write("relay read " + str(relay) + "\n\r")
+
+        response = self.serial_port.read(25)
+        if ((response.find("on") > 0) and (req.data)):
+            state = True
+            response = "Turn on relay " + str(relay)
+
+        elif ((response.find("off") > 0) and (req.data == False)):
+            state = True
+            response = "Turn off relay " + str(relay)
+        else:
+            state = False
+            response = "!! ERROR !! not able to control relay"
+        return SetBoolResponse(state, response)
+
+    def control_relay_write(self, req, relay):
+        if(req.data):
+            cmd = str("on")
+        else:
+            cmd = str("off")
+        self.serial_port.write("relay " + cmd + " " + str(relay) + "\n\r")
+        self.serial_port.flush()
+        return
+    
+    def control_relay_write_array(self, dataArray):
+        print(len(dataArray.data))
+        led = 0
+        for x in dataArray.data:
+            #print(led)
+            if(x==1):
+                cmd = str("on")
+            else:
+                cmd = str("off")
+            self.serial_port.write("relay " + cmd + " " + str(led) + "\n\r")
+            self.serial_port.flush()
+            led = led + 1
+            
+        return    
+        
+    def callbackSetLEDs(self, dataArray): 
+		self.control_relay_write_array(dataArray)
+		return	
+
+    def control_led_0(self, req):
+        return self.control_relay_write_read(req, 0)
+
+    def control_led_1(self, req):
+        return self.control_relay_write_read(req, 1)
+
+    def control_led_2(self, req):
+        return self.control_relay_write_read(req, 2)
+
+    def control_led_3(self, req):
+        return self.control_relay_write_read(req, 3)
+
+    def control_led_4(self, req):
+        return self.control_relay_write_read(req, 4)
+
+    def control_led_5(self, req):
+        return self.control_relay_write_read(req, 5)
+
+    def control_led_6(self, req):
+        return self.control_relay_write_read(req, 6)
+
+    def control_led_7(self, req):
+        return self.control_relay_write_read(req, 7)
+    
+    def control_all_leds(self, req):
+        self.control_relay_write(req, 0)
+        self.control_relay_write(req, 1)
+        self.control_relay_write(req, 2)
+        self.control_relay_write(req, 3)
+        self.control_relay_write(req, 4)
+        self.control_relay_write(req, 5)
+        self.control_relay_write(req, 6)
+        self.control_relay_write(req, 7)
+        response = "All LEDs set to: " + str(req.data)
+        return SetBoolResponse(True, response)
+
+    def control_right_yellow_leds(self, req):
+        self.control_relay_write(req, 3)
+        self.control_relay_write(req, 5)
+        response = "Right Yellow LEDs set to: " + str(req.data)
+        return SetBoolResponse(True, response)
+
+    def control_left_yellow_leds(self, req):
+        self.control_relay_writ(req, 2)
+        self.control_relay_write(req, 4)
+        response = "Left Yellow LEDs set to: " + str(req.data)
+        return SetBoolResponse(True, response)
+
+if __name__ == "__main__":
+    rospy.init_node('Led_control_server')
+    nri = LedControl()
---
+++ scripts/nr_ctrl_cmd.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import struct
+import time
+from std_msgs.msg import UInt8, Int16
+from geometry_msgs.msg import Twist
+from night_rider.msg import CtrlCmd, RecvStatus
+import rospy
+import rospkg
+import message_filters
+
+debug = 0
+packet = [0 for i in range(7)]  # creat array for packet
+
+class nr_serial_test: 
+#=======================================================================
+    def cmd_vel_callback(self, cmd_vel) :
+        
+        #rospy.loginfo(" ROS CALL BACK RECIVED ")
+        #rospy.info(str(cmd_vel))
+				
+		# ---- NR controller mode ------		
+        cmd_mode = 1  		# set mode to 1 = Auto/computer control (O = manual mode for the black controller)
+        
+        # ---- NR eStop state ------
+        cmd_e_stop = 0	    # set to zero at all time by the computer estop is seperate system
+        
+        # ---- control of NR gears ------
+        # To more forward mode need to be "drive"
+        # To more backwards mode need to be "reverse"
+        # safe state is "neutral" where NR will roll if push or under inertia
+        # "cmd_vel.linear.x" is being used for both speed and direction to stay in ROS norms
+        if cmd_vel.linear.x > 0.01 :
+             cmd_gear = 0 				# Drive
+        elif cmd_vel.linear.x < -0.01 :
+            cmd_gear = 3				# Reverse
+            cmd_vel.linear.x = not cmd_vel.linear.x
+        else :
+            cmd_gear = 1				# Neutral
+
+		# ---- NR speed  ------
+		# The NR drive use gears to define forward or backwards
+		# The maxium unit to the NR MCU is 1000 units
+		# Maxium speed of current geared NR is 8.2M/s (~30KM)
+		# "scaler_nr_speed" is to match the MCU units to ROS twist m/s velosicty 
+        cmd_speed = cmd_vel.linear.x * self.scaler_nr_speed 	
+        
+        # ---- NR steer  ------
+        # As NR is Ackermen, cmd_velangular.z this normalized to 1.0 - 0 for maxium & minium steering (+/-28 deg)
+        if cmd_vel.angular.z > 1.0 : cmd_vel.angular.z = 1.0
+        if cmd_vel.angular.z <-1.0 : cmd_vel.angular.z = -1.0
+        cmd_steer = cmd_vel.angular.z * 2000
+        
+        # ---- NR steer  ------
+        # As NR is is using negative "cmd_vel.linear.x" for gear and speed
+        # the "break" will be controlled on cmd_vel.linear.z
+        # if cmd_vel.linear.x = 0 and cmd_vel.linear.z = 0 the NR will be in neutral and just roll to a stop.
+        if cmd_vel.linear.z > 0.01 :
+            if cmd_vel.linear.z > 1 : cmd_vel.linear.z = 1 		# cmd_vel.linear.z normilized to 1.0 - 0 for maxium & minium (100 = full break)
+            cmd_brake = cmd_vel.linear.z * 100
+        else :
+            cmd_brake = 0
+        
+        # --- Tidy & check data ---
+        if cmd_speed > 1000 : cmd_speed = 1000
+        if cmd_speed < 0 : cmd_speed = 0
+        if cmd_steer > 2000 : cmd_steer = 2000
+        if cmd_steer < -2000: cmd_steer = -2000
+        if cmd_brake > 100 : cmd_brake = 100
+        if cmd_brake < 0: cmd_brake = 0
+
+        packet = [cmd_mode, cmd_e_stop, cmd_gear, cmd_speed, cmd_steer, cmd_brake]
+#-----------------------------------------------------------------------
+
+#=======================================================================
+    def pub_cmd(self, test_case_number, serial_msg_hz, mode, e_stop, gear, speed, steer, brake) :
+        ctrl_msg = CtrlCmd()
+        ctrl_msg.test_case = test_case_number
+        ctrl_msg.serial_msg_hz = serial_msg_hz
+        ctrl_msg.mode = mode
+        ctrl_msg.e_stop = e_stop
+        ctrl_msg.gear = gear
+        ctrl_msg.speed = speed
+        ctrl_msg.steer = steer
+        ctrl_msg.brake = brake
+        ctrl_msg.alive = self.alive
+        
+        self.nr_cmd_pub.publish(ctrl_msg)
+#-----------------------------------------------------------------------      
+  
+#=======================================================================
+    def conversion(self, mode, e_stop, gear) :
+        if mode == 0: 
+            mode = "Manual"
+        else:
+            mode = "Auto"
+
+        if e_stop == 0:
+            e_stop = "OFF"
+        else:
+            e_stop = "ON"
+
+        if gear == 0:
+            gear = "Drive"
+        elif gear == 2:
+            gear = "Rear"
+        else:
+            gear = "Neutral"
+
+        return mode, e_stop, gear
+#-----------------------------------------------------------------------
+        
+#=======================================================================
+    def sub_recv_status(self,RecvStatus) :
+        #print("--------------------status(sub)--------------------")
+        #print("recv_status(mode) : {}".format(RecvStatus.mode))
+        #print("recv_status(e_stop) : {}".format(RecvStatus.e_stop))
+        #print("recv_status(gear) : {}".format(RecvStatus.gear))
+        #print("recv_status(speed) : {}".format(RecvStatus.speed))
+        #print("recv_status(steer) : {}".format(RecvStatus.steer))
+        #print("recv_status(brake) : {}".format(RecvStatus.brake))
+        #print("recv_status(enc) : {}".format(RecvStatus.enc))
+        #print("recv_status(batt) : {}".format(RecvStatus.batt))
+        #print("recv_status(alive) : {}".format(RecvStatus.alive))
+        #print("---------------------------------------------------\n")
+        return
+#-----------------------------------------------------------------------
+
+#=======================================================================
+    def main_loop(self, test_case_number, serial_msg_hz) :
+        
+        if debug: print(serial_msg_hz)
+        
+        rate = rospy.Rate(100)
+        t_offset = time.time()
+        t_prev = 0.0
+        t = 0.0
+        self.alive = 0
+        while not rospy.is_shutdown(): 
+            t = time.time() - t_offset
+            t_elapsed = t - t_prev
+ 
+            if t_elapsed > serial_msg_hz:               
+                t_prev = t
+                
+ #               cmd_packet = self.test_case.ctrl_cmd_test_case(test_case_number, t, serial_msg_hz)
+                cmd_packet = packet
+                
+                self.pub_cmd(
+                    test_case_number,
+                    serial_msg_hz,
+                    cmd_packet[0],
+                    cmd_packet[1],
+                    cmd_packet[2],
+                    cmd_packet[3],
+                    cmd_packet[4],
+                    cmd_packet[5],
+                )
+               
+                self.alive = self.alive + 1
+                if self.alive == 256:
+                    self.alive = 0
+            rate.sleep()
+#----------------------------------------------------------------------- 
+
+#=======================================================================
+    def __init__(self):
+        #self.test_case = test_case()
+        rospy.init_node("nr_cmd", anonymous=True)
+        self.serial_msg_hz = rospy.get_param('~serial_msg_hz', 1/20)
+        self.scaler_nr_speed = rospy.get_param('~speed_scaler', 125)  	# 1000 / Max speed(8.2m/s) 
+        self.baud = rospy.get_param('~baud', 115200)
+        
+        
+        self.nr_cmd_pub = rospy.Publisher("/night_rider/ctrl_cmd", CtrlCmd, queue_size=1)
+        self.nr_cmd_vel_sub = rospy.Subscriber("/night_rider/cmd_vel", Twist, self.cmd_vel_callback)
+        self.nr_status_sub = rospy.Subscriber("/night_rider/recv_status", RecvStatus, self.sub_recv_status)
+        self.main_loop(test_case_number, self.serial_msg_hz)
+#-----------------------------------------------------------------------    
+
+if __name__ == "__main__":
+    try:
+        test_case_number = int(input("Change : Mode(1), E-STOP(2), Gear(3), Speed(4), Steer(5), Speed and Steer(6) : "))
+        if test_case_number >= 0 and test_case_number <= 6 :
+            tester = nr_serial_test()
+    except rospy.ROSInterruptException:
+        pass
