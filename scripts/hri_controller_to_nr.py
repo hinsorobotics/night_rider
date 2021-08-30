7b37dd77943a9e6029f80565124547e5ce9715c7
hinsorobotics <hinsorobotics@gmail.com>
2021-08-30 17:58:48 -0400

    led control from HRI controller under development

--- scripts/hri_controller_to_nr.py
+++ scripts/hri_controller_to_nr.py
@@ -16,21 +16,27 @@ import rospy
 from std_msgs.msg import ByteMultiArray
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+from night_rider.msg import LedCmd
 
 class hri_controller_to_nr: 
+#=======================================================================
 	def callbackJoy2twist(self, data):
-		# --- declearations
+#------ declearations
 		twist = Twist()
-		ledArray = ByteMultiArray()
+		
 		
 		# ----- Braking -----
 		# hri publishes on the "joy" topic where the left trigger 
 		# is used as a "death man" / break safety input. Thispull down and 
 		# keep this trigger down to dis-enage the break. If the use is not
 		# focused or is kocked or worse, then this trigger will reset and the 
-		# break will be enaged for safety means the user 
+		# break will be enaged for safety means the user
 		twist.linear.z = 1 - data.buttons[10]
-		
+		if twist.linear.z == 1 :
+			self.led_cmd.rear_red_spot_led = 1
+		else :
+			self.led_cmd.rear_red_spot_led = 0
+			
 		# ----- Speed and Gear -----
 		# hri publishes on the "joy" topic where the left analog stick
 		# is use to deturmian the speed of the NR and
@@ -51,33 +57,54 @@ class hri_controller_to_nr:
 		twist.angular.z = -data.axes[2] 
 		
 		
-		
+		if data.buttons[6] == 1 and self.last_button_state == 0 :
+			self.led_cmd.front_spot_leds = not self.led_cmd.front_spot_leds 
+			print (self.led_cmd.front_spot_leds)
 
-		if data.buttons[6] == 1:
-			ledArray.data[0] = not ledArray.data[0]
+		#if data.buttons[4] == 1 and self.last_button_state[4] == 0 :
+		#	if self.rear_spot_leds == 0 :
+		#		self.led_cmd.rear_spot_leds = 1
+		#		self.front_spot_leds = 1
+		#	elif self.front_spot_leds == 1 :
+		#		self.led_cmd.front_spot_leds = 0
+		#		self.front_spot_leds = 0
+			
 		
-		self.pubLEDs.publish(ledArray)
+		self.last_button_state = data.buttons[6]
+		
+		self.pubLEDs.publish(self.led_cmd)
+		
 		self.pubTwist.publish(twist)
+#-----------------------------------------------------------------------  
 		
-		
-		
+#=======================================================================
 	def main_loop(self) :
 		rospy.loginfo("HRI Joy To Night Rider Node Started")
-		
-		
+
 		while not rospy.is_shutdown(): 
+			
 			rospy.spin()
+#-----------------------------------------------------------------------  
 			
-			
-	# Intializes everything
+#=======================================================================
 	def __init__(self):
 		# publishing to "/cmd_vel" to control night rider
 		global pubTwist, pubLEDs
+		global led_cmd, led_cmd_pervious, front_spot_leds, last_button_state
+		
+		self.front_spot_leds = bool()
+		self.last_button_state = bool()
+		
+		self.front_spot_leds = 0
+		
+		self.led_cmd = LedCmd()
+		self.led_cmd_pervious = LedCmd()
+		
 		
 		rospy.init_node('hri_controller_to_nr')
 		
 		self.pubTwist = rospy.Publisher('/night_rider/cmd_vel', Twist, queue_size=10)
-		self.pubLEDs = rospy.Publisher('/led_light_control', ByteMultiArray, queue_size=1)
+		self.pubLEDs = rospy.Publisher('/led_light_control', LedCmd, queue_size=1)
 		
 		# subscribed to joystick inputs on topic "joy"
 		rospy.Subscriber("joy", Joy, self.callbackJoy2twist)
@@ -88,7 +115,7 @@ class hri_controller_to_nr:
 		
 		# starts the node
 		self.main_loop()
-		
+#-----------------------------------------------------------------------  
 
 if __name__ == '__main__':
     hri_controller_to_nr()
--- scripts/led_control.py
+++ scripts/led_control.py
@@ -15,37 +15,12 @@ import rospy
 import serial
 from std_srvs.srv import SetBool, SetBoolResponse
 from std_msgs.msg import ByteMultiArray
+from night_rider.msg import LedCmd
 
 
-class LedControl:
-    def __init__(self):
-#-------------- Serial setup --------------------------------------------------
-        self.port = rospy.get_param('~port', '/dev/ttyACM3')
-        self.baud = rospy.get_param('~baud', 19200)
-        self.serial_port = serial.Serial(self.port, self.baud, timeout=1)
+class led_control:
 #-------------- function deleceration -----------------------------------------
-        rospy.Service('set_rear_red_led', SetBool, self.control_led_0)
-        rospy.Service('set_rear_spot_leds', SetBool, self.control_led_1)
-        rospy.Service('set_rear_left_yellow_led', SetBool, self.control_led_2)
-        rospy.Service('set_rear_right_yellow_led', SetBool, self.control_led_3)       
-        rospy.Service('set_front_left_yellow_led', SetBool, self.control_led_4)
-        rospy.Service('set_front_right_yellow_led', SetBool, self.control_led_5)
-        rospy.Service('set_front_spot_leds', SetBool, self.control_led_6)
-        rospy.Service('set_spare_led', SetBool, self.control_led_7)
-        rospy.Service('set_all_leds', SetBool, self.control_all_leds)
-        rospy.Service('set_right_yellow_leds', SetBool, self.control_right_yellow_leds)
-        rospy.Service('set_left_yellow_leds', SetBool, self.control_left_yellow_leds)
-        
-        rospy.Subscriber("/led_light_control", ByteMultiArray, self.callbackSetLEDs)
-        
-#-------------- ROS Spin loop -------------------------------------------------
-        while not rospy.is_shutdown():
-            rospy.spin()
-
-        self.serial_port.close()
-
-
-#-------------- function deleceration -----------------------------------------
+#=======================================================================
     def control_relay_write_read(self, req, relay):
         if(req.data):
             cmd = str("on")
@@ -68,6 +43,7 @@ class LedControl:
             response = "!! ERROR !! not able to control relay"
         return SetBoolResponse(state, response)
 
+#=======================================================================
     def control_relay_write(self, req, relay):
         if(req.data):
             cmd = str("on")
@@ -76,26 +52,19 @@ class LedControl:
         self.serial_port.write("relay " + cmd + " " + str(relay) + "\n\r")
         self.serial_port.flush()
         return
-    
-    def control_relay_write_array(self, dataArray):
-        print(len(dataArray.data))
-        led = 0
-        for x in dataArray.data:
-            #print(led)
-            if(x==1):
-                cmd = str("on")
-            else:
-                cmd = str("off")
-            self.serial_port.write("relay " + cmd + " " + str(led) + "\n\r")
-            self.serial_port.flush()
-            led = led + 1
+
+#=======================================================================
+    def control_relay_toggle(self, toggle, relay):
+        if toggle :
+            cmd = str("on")
+        else :
+            cmd = str("off")
+        self.serial_port.write("relay " + cmd + " " + str(relay) + "\n\r")
+        self.serial_port.flush()
             
         return    
-        
-    def callbackSetLEDs(self, dataArray): 
-		self.control_relay_write_array(dataArray)
-		return	
 
+#=======================================================================
     def control_led_0(self, req):
         return self.control_relay_write_read(req, 0)
 
@@ -119,7 +88,8 @@ class LedControl:
 
     def control_led_7(self, req):
         return self.control_relay_write_read(req, 7)
-    
+
+#=======================================================================
     def control_all_leds(self, req):
         self.control_relay_write(req, 0)
         self.control_relay_write(req, 1)
@@ -132,18 +102,72 @@ class LedControl:
         response = "All LEDs set to: " + str(req.data)
         return SetBoolResponse(True, response)
 
+#=======================================================================
     def control_right_yellow_leds(self, req):
         self.control_relay_write(req, 3)
         self.control_relay_write(req, 5)
         response = "Right Yellow LEDs set to: " + str(req.data)
         return SetBoolResponse(True, response)
 
+#=======================================================================
     def control_left_yellow_leds(self, req):
         self.control_relay_writ(req, 2)
         self.control_relay_write(req, 4)
         response = "Left Yellow LEDs set to: " + str(req.data)
         return SetBoolResponse(True, response)
 
+#=======================================================================
+    def callbackSetLEDs(self, msg): 
+
+        self.control_relay_toggle(msg.front_spot_leds, 6)
+        print(msg.front_spot_leds)
+
+            
+        if msg.rear_red_spot_led :   
+            self.control_relay_toggle(1, 0)
+        else :
+            self.control_relay_toggle(0, 0)
+            
+       # self.control_relay_write(req.data=msg.front_right_led, 5)
+       # self.control_relay_write(req.data=msg.front_left_led, 4)
+       # self.control_relay_write(req.data=msg.rear_spot_leds, 1)
+       # self.control_relay_write(req.data=msg.rear_left_led, 2)
+       # self.control_relay_write(req.data=msg.rear_right_led, 3)
+       # self.control_relay_write(req.data=msg.rear_red_spot_led, 0)
+
+#=======================================================================
+    def main_loop(self):
+        rate = rospy.Rate(50)
+        while not rospy.is_shutdown():
+            rate.sleep()
+        self.serial_port.close()
+
+#-----------------------------------------------------------------------
+
+#=======================================================================
+    def __init__(self):
+#-------------- Serial setup --------------------------------------------------
+        self.port = rospy.get_param('~port', '/dev/ttyACM3')
+        self.baud = rospy.get_param('~baud', 19200)
+        self.serial_port = serial.Serial(self.port, self.baud, timeout=1)
+#-------------- function deleceration -----------------------------------------
+        rospy.Service('set_rear_red_led', SetBool, self.control_led_0)
+        rospy.Service('set_rear_spot_leds', SetBool, self.control_led_1)
+        rospy.Service('set_rear_left_yellow_led', SetBool, self.control_led_2)
+        rospy.Service('set_rear_right_yellow_led', SetBool, self.control_led_3)       
+        rospy.Service('set_front_left_yellow_led', SetBool, self.control_led_4)
+        rospy.Service('set_front_right_yellow_led', SetBool, self.control_led_5)
+        rospy.Service('set_front_spot_leds', SetBool, self.control_led_6)
+        rospy.Service('set_spare_led', SetBool, self.control_led_7)
+        rospy.Service('set_all_leds', SetBool, self.control_all_leds)
+        rospy.Service('set_right_yellow_leds', SetBool, self.control_right_yellow_leds)
+        rospy.Service('set_left_yellow_leds', SetBool, self.control_left_yellow_leds)
+        
+        rospy.Subscriber("/led_light_control", LedCmd, self.callbackSetLEDs)
+        
+        self.main_loop()
+#-----------------------------------------------------------------------
+
 if __name__ == "__main__":
     rospy.init_node('Led_control_server')
-    nri = LedControl()
+    nri = led_control()
